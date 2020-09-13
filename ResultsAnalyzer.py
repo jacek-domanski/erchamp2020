15208979e22ecf1d89002e3df2aed80f71419f9d
jacek-domanski <j.domanski92@gmail.com>
2020-09-13 13:37:12 +0200

    :tada: Add empty ResultsAnalyzer class

---
+++ ResultsAnalyzer.py
@@ -0,0 +1,13 @@
+
+
+class ResultsAnalyzer:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    results_analyzer = ResultsAnalyzer()
+    results_analyzer.run()
